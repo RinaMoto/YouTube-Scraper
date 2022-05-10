a7c0d165abcb34dc5645d0b9935e98fd9130699f
Rina Easterday <rina.easterday@gmail.com>
2022-05-09 21:07:18 -0700

    Attemping with methods included

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/videos')
+@app.route('/videos', methods=['POST'])
 def get_vids():
     artist = request.get_json()
     urls = []
